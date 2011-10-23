2f5d6ea5479664295c4132296515e2384218d1d4
technocake <technocake@0a2b00dd-c9bf-4efb-bb1d-7794fe0c548a>
2011-10-23 23:43:19 +0000

    Functioning With timer finally :D

--- netchallonged/netchallonged.py
+++ netchallonged/netchallonged.py
@@ -5,8 +5,14 @@ import random
 import time
 import threading
 import math
+#Custom timer :P
+import timer
 
-import SocketServer
+try:
+	import SocketServer
+except:
+	import socketserver as SocketServer
+
 from prompt import *
 #CONFIGZ
 #http://docs.python.org/library/socketserver.html
@@ -32,13 +38,16 @@ def getChallonge():
 	#	Will yield something like: 13+37
 	return "%d%s%d" % (ai, operators[oi], bi)
 
+	
 
 
 class NerdHandler(SocketServer.StreamRequestHandler):
 	def handle(self):
 		cur_thread = threading.currentThread()
-		print ("%s Joined. Will he or she manage? Assigned thread: %s " %  (self.client_address[0], cur_thread) )
+		print ("%s Joined. Will he or she manage? The clock is ticking." %(self.client_address[0]))
 		try:
+			#Limit of 1 sec
+			t = timer.Timer(1)
 			# Getting the language the nerd is using 
 			language=self.rfile.readline().strip()
 		
@@ -47,17 +56,19 @@ class NerdHandler(SocketServer.StreamRequestHandler):
 			# We need to know the answer to see if the nerd did it..
 			answer = int( eval(challenge) )
 			
-			self.wfile.write(challenge + "\n")
+			self.SaySomething(challenge + "\n")
 			# 32bit integer max 
-			nerdAttempt = int ( self.rfile.readline( int( math.pow(2,32) ) ).strip() )
+			nerdAttempt = int ( self.rfile.readline( int( math.pow(2,30) ) ).strip() )
 			
 			#Did he make the challenge?
 			passed = answer == nerdAttempt
 			
 			#Telling him/her:
-			reply = "Correct!\n" if passed else "Wrong!\n"
-			self.wfile.write(reply)
-			
+			reply = "Correct!\n" if passed and t.timeLeft() else "Wrong!\n"
+			self.SaySomething(reply)
+		
+			# http://effbot.org/zone/thread-synchronization.htm
+		
 			print ( "The nerd gave an attempt to answer the challonge. \n \
 			\t Challenge: \t %s \n \
 			. He thought it to be:  %d \n \
@@ -65,10 +76,14 @@ class NerdHandler(SocketServer.StreamRequestHandler):
 			, and he is  %s !!!!" % 
 				(challenge, nerdAttempt, answer, reply) )
 				
-		except:
-			print ( "Man quit! %s "%(e,))
+		except Exception as e:
+			print ( "Man quit! %s" %(e,))
 		finally:
 			print ( "%s Nerd Lost:/ " % self.client_address[0] )
+	
+	def SaySomething(self, something):
+		""" Python 3 compability issue handling :) Str is no longar  string or something"""
+		self.wfile.write(bytes(something, 'UTF-8'))
 
 class ThreadedNetChallonged(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 	pass
@@ -92,7 +107,7 @@ if __name__ == "__main__":
 		serverThread.setDaemon(True)
 		serverThread.start()
 		while 1:
-			if "quit" in prompt("YoMamaD>> "):
+			if "quit" in prompt("Code::Phun->NetChallongeD>> "):
 				shutUp()
 	except KeyboardInterrupt:
 		print ( "Shuting down, erh up... ")
