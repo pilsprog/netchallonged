9332f460d193c419d0c1f27b607e041733802623
root <root@komsys.org>
2012-02-24 20:31:25 +0100

    Added the web-ui interface files ...  It consist of the index.php

---
+++ web-ui/index.php
@@ -0,0 +1,65 @@
+<!Doctype html>
+<html>
+<head>
+<style>
+	body {
+		background-color: #6f3636;
+		background-image: url('codephun.com.bg.png');
+		background-repeat: no-repeat;
+		margin: 0 auto;
+		background-position: top center;
+		font-family: tahoma;
+		background-size: 100%;
+}
+
+	.artCont {
+		background: #fcfbd2;
+		opacity:0.55;	
+		margin: 350px auto;
+		width: 500px;
+		border: 5px solid white;
+	}
+
+		
+	.article {
+		opacity: 1.00;
+		text-align: center;
+		width: 500px;
+		margin: 0 auto;
+	}
+</style>
+</head>
+<body>
+
+	<div class="artCont">
+	<div class="article"> 
+		<h1> Network Challonge Alive! </h1> 
+		<p> 
+	Challenge: Make a program that connects to pompel.komsys.org:1337 using a tcp socket and does the following:  
+	<ul><li> 
+	1. sends your nickname.</li> 
+	<li> 2. receives a math challenge. </li> 
+	<li>3. Sends the answer to that challeng in less then one second! </li> 
+	</ul> 
+	<p> 
+		Example of math challenge: 729*632 
+	</p>
+ 
+	</p>
+
+	</div>
+	<div class="article">
+	<h1> LVL 1: Backlengs challonge Alive! </h1>
+	<h3>
+	Challenge: Make a program that connects to pompel.komsys.org:1337  using a tcp socket and does the following: </h3>
+	<ul>
+	<li>		1. sends your nickname. </li>
+	<li>		2. receives a random yo-mama joke, and then flips it backwords. So yo-mama is fat --> taf si amam-oy .</li> 
+	<li>		3. Sends the answer to that challenge in less then 1 second!</li>
+
+	</ul>
+	</div>
+</div>
+	</div>
+
+<body>
