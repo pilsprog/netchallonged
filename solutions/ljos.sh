59c51802e8e3549e1a0c24f7475896324629c0c6
Bjarte Johansen <bjo013@uib.no>
2012-04-27 20:01:29 +0200

    updated to newest version

--- solutions/ljos.sh
+++ solutions/ljos.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 # @author ljos
-
-exec 3<>/dev/tcp/pompel.komsys.org/1337;echo ljos>&3;read m<&3;echo $[$m]>&3;read m<&3;echo $m
-
-
+exec 3<>/dev/tcp/2653248386/1337;echo ljos>&3;read m<&3;echo $[$m]>&3;read m<&3;echo $m
